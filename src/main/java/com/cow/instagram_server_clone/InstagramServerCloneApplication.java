0ae7032390941447ec8dd0bb1b10570b5409009d
jinhyeon <tiemo0708@naver.com>
2024-07-02 20:53:26 +0900

    feat: 초기 설정

---
+++ src/main/java/com/cow/instagram_server_clone/InstagramServerCloneApplication.java
@@ -0,0 +1,12 @@
+package com.cow.instagram_server_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramServerCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramServerCloneApplication.class, args);
+    }
+}
